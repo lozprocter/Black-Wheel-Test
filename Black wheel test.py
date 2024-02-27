8939b369fcffba608b7727ed55d749ddc8f5f801
lozprocter <72624353+lozprocter@users.noreply.github.com>
2024-02-27 13:17:06 +0000

    Add files via upload

---
+++ Black wheel test.py
@@ -0,0 +1,19 @@
+#PARAMETER INPUTS
+hours = 10
+
+
+#STATIC PARAMETERS - DO NOT CHANGE
+y_distance_mm = 1200
+run_time_millis = 5000
+milliseconds = (hours*60*60*1000)
+run_count = int(milliseconds // run_time_millis)
+
+with open ('C:\\Users\\ramiz\\Documents\\Black wheel test.gcode','w') as f:
+    f.write("G0\n")
+    f.write("G91\n")
+
+    for i in range (0, (run_count+1)):
+        f.write("Y1200 F6000\n")
+        f.write("Y-1200 F6000\n")
+
+f.close()
