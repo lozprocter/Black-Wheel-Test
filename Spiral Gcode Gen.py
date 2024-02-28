22441af0ab62f623143162a4a78ee852f8748f96
Loz Procter <lozprocter@yahoo.co.uk>
2024-02-28 14:28:15 +0000

    added spindle speed and feedrate to spiral

--- Spiral Gcode Gen.py
+++ Spiral Gcode Gen.py
@@ -2,6 +2,8 @@
 step_down = 6
 tool_dia = 6
 passes = 2
+feedrate = 4000
+spindle_speed = 22500
 min_line_length = 5
 max_line_length = 50
 
@@ -17,6 +19,7 @@ with open ('C:\\Users\\USER\\Documents\\Black-Wheel-Test\\Output\\Spiral.gcode',
     f.write("G0\n")
     f.write("G90\n")
     f.write("X0 Y0 Z5\n")
+    f.write(f"M3 S{spindle_speed}")
     f.write("G0\n")
     f.write("G91\n")
     f.write("Z-5\n")
@@ -24,10 +27,14 @@ with open ('C:\\Users\\USER\\Documents\\Black-Wheel-Test\\Output\\Spiral.gcode',
     while passes_complete < passes:
         f.write(f"Z-{step_down}\n")
         for i in range(min_line_length, max_line_length, step_over*2):
-            f.write(f"X{i} F6000\n")
-            f.write(f"Y{i} F6000\n")
-            f.write(f"X-{i+step_over} F6000\n")
-            f.write(f"Y-{i+step_over} F6000\n")
+            f.write(f"X{i} F{feedrate}\n")
+            f.write(f"Y{i} F{feedrate}\n")
+            f.write(f"X-{i+step_over} F{feedrate}\n")
+            f.write(f"Y-{i+step_over} F{feedrate}\n")
         passes_complete += 1
+
+    f.write("G90")
+    f.write("Z50")
+    f.write("M5")
 
 f.close()
